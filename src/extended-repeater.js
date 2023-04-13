1f8ced16756d794e954baafd4d8757a4ec27d044
Dmitriy Bulgakov <4irtim@gmail.com>
2023-04-13 23:04:10 +0200

    feat: extended-repeater solved

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+// str is a string to repeat;
+// options is an object of options, that contains properties:
+// repeatTimes sets the number of repetitions of the str;
+// separator is a string separating repetitions of the str;
+// addition is an additional string that will be added to each repetition of the str;
+// additionRepeatTimes sets the number of repetitions of the addition;
+// additionSeparator is a string separating repetitions of the addition.
+
+
+function repeater(str, options) {
+  options.separator = options.separator||'+';
+  options.additionSeparator = options.additionSeparator||'|'
+  options.repeatTimes = options.repeatTimes||1;
+  options.additionRepeatTimes = options.additionRepeatTimes||1;
+  str === null ? str = 'null': '';
+  options.addition === null ? options.addition = 'null' : '';
+  let repeat = (s, rt, sep) => Array.from({length: rt}, (v, i) => s).join(sep);
+  let add = repeat(options.addition, options.additionRepeatTimes, options.additionSeparator);
+  return repeat(str + add, options.repeatTimes, options.separator);
 }
 
 module.exports = {
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -44,16 +44,3 @@ const chainMaker = {
 module.exports = {
   chainMaker
 };
-
-// let a = chainMaker.reverseChain().reverseChain().reverseChain().addLink(NaN).reverseChain().addLink(null).addLink(1.233).addLink(true).addLink(false).removeLink(3).addLink(1.233).finishChain()
-// // , '( NaN )~~( null )~~( true )~~( false )~~( 1.233 )');
-// let b = chainMaker.reverseChain().addLink('ABC').reverseChain().reverseChain().reverseChain().addLink(Infinity).addLink(false).addLink(0).addLink('8.963').removeLink(2).removeLink(1).reverseChain().finishChain()
-// // , '( 8.963 )~~( 0 )~~( false )');
-// let c = chainMaker.addLink(null).addLink(false).addLink(22).reverseChain().reverseChain().removeLink(2).reverseChain().reverseChain().addLink({ 0: 'first', 1: 'second', 'length': 2 }).reverseChain().addLink('DEF').finishChain()
-// // , '( [object Object] )~~( 22 )~~( null )~~( DEF )');
-// let d = chainMaker.addLink(3.14).addLink(1).addLink({ 0: 'first', 1: 'second', 'length': 2 }).removeLink(1).addLink('DEF').addLink({ 0: 'first', 1: 'second', 'length': 2 }).removeLink(1).addLink(true).addLink(false).addLink(333).reverseChain().reverseChain().finishChain()
-// // , '( [object Object] )~~( DEF )~~( [object Object] )~~( true )~~( false )~~( 333 )');
-// let e =chainMaker.addLink('ABC').reverseChain().reverseChain().addLink('DEF').removeLink(1).addLink({ 0: 'first', 1: 'second', 'length': 2 }).reverseChain().addLink(1.233).addLink(1.233).reverseChain().addLink('ABC').finishChain()
-// // , '( 1.233 )~~( 1.233 )~~( DEF )~~( [object Object] )~~( ABC )');
-//let b = chainMaker.reverseChain().addLink('ABC').reverseChain().reverseChain().reverseChain().addLink(Infinity).addLink(false).addLink(0).addLink('8.963').removeLink(2).removeLink(1).reverseChain().finishChain();
-// console.log(e);
